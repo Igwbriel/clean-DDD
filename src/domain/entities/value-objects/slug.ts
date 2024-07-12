4e0fe3cac9a104b57d5c9a55e1ce14d914270757
Gabriel Dias <gabrieldias7820@gmail.com>
2024-07-12 10:03:18 -0300

    value objects de slug

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,21 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  static createFromtext(text: string){
+    const slugText = text
+    .normalize("NFKD") 
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-') // espaços em branco
+    .replace(/[^\w-]+/g, '') // o que não são palavras
+    .replace(/_/g, '-') // substituir um _ por -
+    .replace(/--+/g, '-') // substituir -- por -
+    .replace(/-$/g, '') // tirar os - no final da slug  
+
+    return new Slug(slugText)
+  }
+}
